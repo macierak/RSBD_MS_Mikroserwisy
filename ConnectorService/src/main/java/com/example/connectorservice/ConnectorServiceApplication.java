d4e158689348a3a32909492568069a8a78147875
Maciej Wojdyła <maciejwojdyla@10g.pl>
2023-01-15 15:02:39 +0100

    swagger

--- ConnectorService/src/main/java/com/example/connectorservice/ConnectorServiceApplication.java
+++ ConnectorService/src/main/java/com/example/connectorservice/ConnectorServiceApplication.java
@@ -3,8 +3,10 @@ package com.example.connectorservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableFeignClients
+@EnableWebMvc
 @SpringBootApplication(scanBasePackages = {"com.example.bugzordclient", "com.example.connectorservice", "com.example.krzysztofclient"})
 public class ConnectorServiceApplication {
 
